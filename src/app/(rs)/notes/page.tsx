009294ce7add55204d88fd70afb4d7337e9a8ff3
Marcell Vajda <vajdam96@gmail.com>
2025-02-05 22:42:06 +0100

    Add button,basic routing

---
+++ src/app/(rs)/notes/page.tsx
@@ -0,0 +1,3 @@
+export default function Notes() {
+  return <h2>Notes Page</h2>;
+}
---
+++ src/app/(rs)/template.tsx
@@ -0,0 +1,9 @@
+// Will rerender every time
+
+export default async function Template({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return <div className="animate-appear">{children}</div>;
+}
