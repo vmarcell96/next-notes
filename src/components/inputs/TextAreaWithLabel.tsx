d674c7c60548502d071f22d62a1643ade029506d
Marcell Vajda <vajdam96@gmail.com>
2025-02-09 20:37:08 +0100

    Add components

---
+++ src/components/inputs/TextAreaWithLabel.tsx
@@ -0,0 +1,58 @@
+"use client";
+
+import { useFormContext } from "react-hook-form";
+
+import {
+    FormControl,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from "@/components/ui/form";
+import { Textarea } from "@/components/ui/textarea";
+import { TextareaHTMLAttributes } from "react";
+
+type Props<S> = {
+    fieldTitle: string;
+    // This makes sure we can only provide
+    // props that are properties of the generic type
+    nameInSchema: keyof S & string;
+    className?: string;
+} & TextareaHTMLAttributes<HTMLTextAreaElement>;
+
+// Generic S
+export function TextAreaWithLabel<S>({
+    fieldTitle,
+    nameInSchema,
+    className,
+    ...props
+}: Props<S>) {
+    const form = useFormContext();
+
+    return (
+        <FormField
+            control={form.control}
+            name={nameInSchema}
+            render={({ field }) => (
+                <FormItem>
+                    <FormLabel
+                        className="text-base mb-2"
+                        htmlFor={nameInSchema}
+                    >
+                        {fieldTitle}
+                    </FormLabel>
+
+                    <FormControl>
+                        <Textarea
+                            id={nameInSchema}
+                            className={`disabled:text-blue-500 dark:disabled:text-yellow-300 disabled:opacity-75 ${className}`}
+                            {...props}
+                            {...field}
+                        />
+                    </FormControl>
+                    <FormMessage />
+                </FormItem>
+            )}
+        />
+    );
+}
