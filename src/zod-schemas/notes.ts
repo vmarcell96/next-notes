beabd96c96157b2aeaf9bdaadb713ca63dd5fcc1
Marcell Vajda <vajdam96@gmail.com>
2025-02-25 19:46:23 +0100

    Refactor note schemas

--- src/zod-schemas/notes.ts
+++ src/zod-schemas/notes.ts
@@ -1,12 +1,30 @@
 import { createInsertSchema, createSelectSchema } from "drizzle-zod";
 import { notes } from "@/db/schema";
+import { z } from "zod";
 
+//Insert
 export const insertNoteSchema = createInsertSchema(notes, {
-  text: (schema) => schema.min(1, "Note content is required"),
+    text: (schema) => schema.min(1, "Note content is required"),
 });
 
-export const selectNoteSchema = createSelectSchema(notes);
+export const insertNoteWithTagsSchema = insertNoteSchema.extend({
+    tagLabels: z.array(z.string()).optional(),
+});
+
+export type insertNoteWithTagsSchemaType =
+    typeof insertNoteWithTagsSchema._type;
 
 export type insertNoteSchemaType = typeof insertNoteSchema._type;
 
+//Select
+
+export const selectNoteSchema = createSelectSchema(notes);
+
+export const selectNoteWithTagsSchema = selectNoteSchema.extend({
+    tagLabels: z.array(z.string()).optional(),
+});
+
 export type selectNoteSchemaType = typeof selectNoteSchema._type;
+
+export type selectNoteWithTagsSchemaType =
+    typeof selectNoteWithTagsSchema._type;
