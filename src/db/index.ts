f48b99e6dab89d2ccb68ce6c52be70f826ecd653
Marcell Vajda <vajdam96@gmail.com>
2025-02-25 19:48:10 +0100

    Use connectionpool so transactions can be used

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,12 +1,25 @@
-import { drizzle } from "drizzle-orm/neon-http";
-import { neon } from "@neondatabase/serverless";
+// import { drizzle } from "drizzle-orm/neon-http";
+// import { neon } from "@neondatabase/serverless";
+// import { config } from "dotenv";
+
+// config({ path: ".env.local" });
+
+// const sql = neon(process.env.DATABASE_URL!);
+
+// // const db = drizzle(sql, { logger: true });
+// const db = drizzle(sql);
+
+// export { db };
+
+import { drizzle } from "drizzle-orm/neon-serverless";
+import { Pool } from "@neondatabase/serverless";
 import { config } from "dotenv";
+import * as schema from "./schema";
 
 config({ path: ".env.local" });
 
-const sql = neon(process.env.DATABASE_URL!);
+const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+// pool.on("error", (err) => console.error(err));
 
 // const db = drizzle(sql, { logger: true });
-const db = drizzle(sql);
-
-export { db };
+export const db = drizzle({ client: pool, schema: schema });
