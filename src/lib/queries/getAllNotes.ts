6b1da9f008fb6d3ddef3a9c6f4df8aed34cda0f1
Marcell Vajda <vajdam96@gmail.com>
2025-02-25 19:47:06 +0100

    Delete unnecessary files

--- src/lib/queries/getAllNotes.ts
+++
@@ -1,9 +0,0 @@
-import { db } from "@/db";
-import { notes } from "@/db/schema";
-
-export async function getAllNotes() {
-  //returns an array
-  const allNotes = await db.select().from(notes);
-
-  return allNotes[0];
-}
