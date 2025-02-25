2ddce4413d40f73761082b62f91b7f8a58735867
Marcell Vajda <vajdam96@gmail.com>
2025-02-25 19:49:27 +0100

    Reinit migration

---
+++ src/db/migrations/0000_nice_proemial_gods.sql
@@ -0,0 +1,24 @@
+CREATE TABLE "note_tags" (
+	"note_id" integer NOT NULL,
+	"tag_id" integer NOT NULL,
+	CONSTRAINT "note_tags_note_id_tag_id_pk" PRIMARY KEY("note_id","tag_id")
+);
+--> statement-breakpoint
+CREATE TABLE "notes" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"text" text,
+	"creator_id" varchar DEFAULT 'anonym' NOT NULL,
+	"created_at" timestamp DEFAULT now() NOT NULL,
+	"updated_at" timestamp DEFAULT now() NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE "tags" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"label" varchar NOT NULL,
+	"creator_id" varchar DEFAULT 'anonym' NOT NULL,
+	"created_at" timestamp DEFAULT now() NOT NULL,
+	"updated_at" timestamp DEFAULT now() NOT NULL
+);
+--> statement-breakpoint
+ALTER TABLE "note_tags" ADD CONSTRAINT "note_tags_note_id_notes_id_fk" FOREIGN KEY ("note_id") REFERENCES "public"."notes"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "note_tags" ADD CONSTRAINT "note_tags_tag_id_tags_id_fk" FOREIGN KEY ("tag_id") REFERENCES "public"."tags"("id") ON DELETE no action ON UPDATE no action;
